2a6a477859c95c5106e99f460aa9d80bc38b104b
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2024-09-02 20:13:20 +0200

    directional sprite 2d rect

--- addons/directional_sprites/directional_sprite_2d.gd
+++ addons/directional_sprites/directional_sprite_2d.gd
@@ -3,6 +3,7 @@
 extends Sprite2D
 class_name DirectionalSprite2D
 
+
 ## The number of directions each frame contains (in left-to-right direction)
 @export var directions: int = 1:
 	set(value):
@@ -10,6 +11,7 @@ class_name DirectionalSprite2D
 		DirectionalHelpers.set_shader_param(material, "directions", value)
 		if Engine.is_editor_hint():
 			notify_property_list_changed ( )
+		item_rect_changed.emit()
 
 ## The orientation the directions are in the Frames
 ## 1 is vertical, 0 is horizontal
@@ -20,15 +22,17 @@ class_name DirectionalSprite2D
 		material.set_shader_parameter("orientation", send)
 		if Engine.is_editor_hint():
 			notify_property_list_changed ( )
+		item_rect_changed.emit()
 
 
 func _set(property: StringName, value: Variant) -> bool:
+	print("var set")
 	#Animation-related props require rebuilding frames
 	# NOT REQUIRED, already handled...
 	#if property == "hframes" || property == "vframes" || property == "frame" || property == "frame_coords":
 	if property in DirectionalHelpers.props:
 		DirectionalHelpers.set_shader_param(material, property, value)
-		
+		item_rect_changed.emit()
 	return false
 
 @onready var shader = preload("DirectionalSprite2DShader.gdshader")
@@ -37,6 +41,7 @@ func _set(property: StringName, value: Variant) -> bool:
 
 func _texture_changed():
 	DirectionalHelpers.set_shader_param(material, "frame", texture)
+	item_rect_changed.emit()
 
 func _ready() -> void:
 	material = ShaderMaterial.new()
@@ -47,3 +52,20 @@ func _ready() -> void:
 	DirectionalHelpers.set_shader_param(material, "frame", texture)
 	DirectionalHelpers.set_shader_param(material, "orientation", orientation)
 	DirectionalHelpers.set_shader_param(material, "directions", directions)
+
+##Rect is not modified by the node's transform (rotation, global position, scale)
+## Note that Region is not supported, as it is not supported by the shaders
+func get_rect() -> Rect2:
+	var rect = super()
+	if not texture:
+		return Rect2(Vector2.ZERO, Vector2.ONE)
+	
+	if orientation == Orientation.HORIZONTAL:
+		rect.size.x /= directions
+		rect.position.x /= directions
+	else:
+		rect.size.y /= directions
+		rect.position.y /= directions
+
+	return rect
+	
